77a6f6582be5ab562934dbdbd386fec1a10a9f7b
Victor Aranha <aldo.victor1610@gmail.com>
2026-02-25 00:57:59 -0300

    musica dos creditos gerenciada pelo singleton

--- carimbo-game/scripts/singletons/sound_manager.gd
+++ carimbo-game/scripts/singletons/sound_manager.gd
@@ -1,16 +1,33 @@
 extends Node
 
+const NUMERO_AUDIOS_SIMULTANEOS = 5
+
 var sfx_players: Array[AudioStreamPlayer] = []
+var _musica_player: AudioStreamPlayer 
 
 func _ready() -> void:
-	for i in 5:
+	_musica_player = AudioStreamPlayer.new()
+	_musica_player.bus = "Música"
+	add_child(_musica_player)
+	
+	for i in NUMERO_AUDIOS_SIMULTANEOS:
 		var player = AudioStreamPlayer.new()
+		player.bus = "Efeitos sonoros"
 		add_child(player)
 		sfx_players.append(player)
-		
+
 func play_sfx(stream: AudioStream):
 	for player in sfx_players:
 		if not player.playing:
 			player.stream = stream
 			player.play()
 			return
+
+func change_musica(stream: AudioStream, esperaAcabar = false):
+	if (esperaAcabar):
+		await _musica_player.finished
+	_musica_player.stream = stream
+	_musica_player.play()
+
+func stop_musica():
+	_musica_player.stop()
--- carimbo-game/scripts/tela_creditos.gd
+++ carimbo-game/scripts/tela_creditos.gd
@@ -9,11 +9,13 @@ var _terminou_fadeout := false
 
 @onready var tela_creditos: Control = $"."
 @onready var margin: MarginContainer = $margin
+const THEME = preload("uid://knjigrxm8wvv")
 
 func _ready() -> void:
 	pos_inicial_credito = tela_creditos.position.y
 	pos_final_credito = tela_creditos.size.y + pos_inicial_credito
 	margin.position.y = pos_final_credito
+	SoundManager.change_musica(THEME)
 
 func _process(delta: float) -> void:
 	var bottom_position_credito = margin.size.y + margin.position.y
@@ -26,10 +28,12 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if (event is InputEventKey || (event is InputEventMouseButton && event.pressed)):
 		if (Input.is_action_pressed("ui_cancel")):
+			SoundManager.stop_musica()
 			saiu_dos_creditos.emit()
 
 func _exibir_fadeout() -> void:
 	var tween = create_tween()
 	tween.tween_property(margin, "modulate:a", 0, 2.5).set_ease(Tween.EASE_OUT)
 	await tween.finished
+	SoundManager.stop_musica()
 	saiu_dos_creditos.emit()
